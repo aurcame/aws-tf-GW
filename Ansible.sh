8a3453cae6ca8edf7fd11cfcfd241990d06187be
aurcame <aurcame@gmail.com>
2020-07-28 15:30:06 +0300

    - Correct syntax

--- Ansible.sh
+++ Ansible.sh
@@ -9,10 +9,16 @@ cd /home/ec2-user/
 
 # cloning Graduation work repository
 git clone https://github.com/aurcame/GW-cicd.git
+
+# change permitions to ec2-user
+chown -R ec2-user:ec2-user GW-cicd/
 cd GW-cicd/playbooks/
 
+# cat /home/dell/.ssh/awskey_frankfurt > /home/ec2-user/.ssh/awskey_frankfurt
+#chmod 0600 /home/ec2-user/.ssh/awskey_frankfurt
+
 # run Ansible script for DevTools instance
-ansible-playbook devtools.yaml -v
+#ansible-playbook devtools.yaml -v
 
 # run Ansible script for CI and QA instances
-ansible-playbook qa_ci_docker.yml -v
+#ansible-playbook qa_ci_docker.yml -v
