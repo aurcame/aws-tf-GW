a8c17b3f76ab5b42f27157a96f505eff1ccfbfc1
aurcame <aurcame@gmail.com>
2020-07-30 19:16:42 +0300

    Change path to yaml manifests;

--- userdata/Ansible.sh
+++ userdata/Ansible.sh
@@ -5,6 +5,10 @@ yum install -y epel-release-latest-7.noarch.rpm
 yum update -y && yum upgrade -y
 yum install -y git python python-devel python-pip openssl ansible tree mc
 
+# add dns names fo Jenkins and Nexus
+echo "127.0.0.1       devtools.jenkins" >> /etc/hosts
+echo "127.0.0.1       devtools.nexus" >> /etc/hosts
+
 cd /home/ec2-user/
 
 # cloning Graduation work repository
@@ -12,7 +16,7 @@ git clone https://github.com/aurcame/GW-cicd.git
 
 # change permitions to ec2-user
 chown -R ec2-user:ec2-user GW-cicd/
-cd GW-cicd/playbooks/
+cd GW-cicd
 
 # cat /home/dell/.ssh/awskey_frankfurt > /home/ec2-user/.ssh/awskey_frankfurt
 #chmod 0600 /home/ec2-user/.ssh/awskey_frankfurt
